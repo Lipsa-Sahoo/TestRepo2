594f96b9d201b83271162f97ad26df1e6538934e
Lipsa Sahoo <lisahoo@adobe.com>
2019-02-25 11:59:18 +0530

    initial commit

---
+++ src/test/java/com/qa/testcases/DataProviderHashMap.java
@@ -0,0 +1,13 @@
+package com.qa.testcases;
+
+import org.testng.annotations.Test;
+
+public class DataProviderHashMap {
+/*public static void main(String[] args) {
+	System.out.println("Hello");
+}*/
+	@Test
+	public void HelloTest() {
+		System.out.println("TestNG ...........success");
+	}
+}
