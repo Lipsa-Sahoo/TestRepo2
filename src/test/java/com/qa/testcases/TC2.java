f056ab3c77f30a0306cd161e72e910f349da6c63
Lipsa Sahoo <lisahoo@adobe.com>
2019-02-27 16:49:12 +0530

    updating to mock conflicts

--- src/test/java/com/qa/testcases/TC2.java
+++ src/test/java/com/qa/testcases/TC2.java
@@ -1,5 +1,9 @@
 package com.qa.testcases;
 
 public class TC2 {
-
+	public static void main(String[] args) {
+		System.out.println("Test2 classes");
+		System.out.println("Test2 classes");
+		System.out.println("Test2 classes");
+	}
 }
