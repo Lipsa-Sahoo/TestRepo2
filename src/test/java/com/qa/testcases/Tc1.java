f4fc708d88483175b94d705b4a77e24ade192fc8
lipsa676 <38936424+Lipsa-Sahoo@users.noreply.github.com>
2019-02-27 16:45:26 +0530

    Update Tc1.java

--- src/test/java/com/qa/testcases/Tc1.java
+++ src/test/java/com/qa/testcases/Tc1.java
@@ -3,5 +3,8 @@ package com.qa.testcases;
 public class Tc1 {
 	public static void main(String[] args) {
 		System.out.println("Test1 classes");
+		System.out.println("Test1 classes");
+		System.out.println("Test1 classes");
+		System.out.println("Test1 classes");
 	}
 }
